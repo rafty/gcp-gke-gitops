3daa46f2e31a5eb8b7b1690e229952392c24b918
yagita <yagita.takashi@gmail.com>
2021-02-25 11:54:45 +0900

    Dockerfileのcopy path変更

---
+++ test/test_app1.py
@@ -0,0 +1,12 @@
+import unittest
+from app.app import hello
+
+
+class TestHelloApp(unittest.TestCase):
+
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello World!\n")
+
+
+if __name__ == '__main__':
+    unittest.main()
